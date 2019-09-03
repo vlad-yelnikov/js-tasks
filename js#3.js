3c22ec11568bd5bfcc7a30b7f2d1d28bad16e967
vlad-yelnikov <vlad.yelnikov@gmail.com>
2019-09-03 23:47:03 +0300

    js#3 completed

--- js#3.js
+++ js#3.js
@@ -1,3 +1,5 @@
+'use strict';
+
 //#5
 
 function lengthSection(a, b) {
@@ -29,4 +31,45 @@ function sum(a, b) {
         result += i + 1;
     }
     console.log(result);
+}
+
+//#8
+
+function factorial(n) {
+    let result = 1;
+    for (let i = 1; i < n + 1; i++) {
+        result *= i;
+    }
+    console.log(result);
+}
+
+//#9
+
+function arraySum() {
+    let num;
+    let myArray = [];
+    do {
+        num = prompt('Enter a number', '');
+        if (num === '' || num === null || isNaN(num)) break;
+        myArray.push(+num);
+    } while (true);
+    console.log(myArray.reduce((sum, num) => sum + num));
+}
+
+//#10
+
+function numbers(a, b) {
+    for (let i = a; i <= b; i++) {
+        let res = '';
+        for (let j = i - a; j >= 0; j--) res += i;
+        console.log(res);
+    }
+}
+
+//#15
+
+function digitN(k, n) {
+    for (let i = 1; i <= n - 1; i++) k /= 10;
+    if (k >= 1) console.log(parseInt(k % 10));
+    else console.log(-1);
 }
