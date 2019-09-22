ab3b0e0964cb49c0107904a06f9bfbd186c52813
vlad-yelnikov <vlad.yelnikov@gmail.com>
2019-09-22 18:26:04 +0300

    add oop-2

---
+++ objects/oop-2.js
@@ -0,0 +1,16 @@
+'use strict';
+
+//1
+
+let calculator = {
+    read(a, b) {
+        this.a = a;
+        this.b = b;
+    },
+    sum() {
+        return this.a + this.b;
+    },
+    mul() {
+        return this.a * this.b;
+    }
+};
