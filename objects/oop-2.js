955c608f57cb06577cdfd6fca3c422866577da27
vlad-yelnikov <vlad.yelnikov@gmail.com>
2019-09-23 23:47:38 +0300

    added 2 and 4 to oop-2

--- objects/oop-2.js
+++ objects/oop-2.js
@@ -14,3 +14,27 @@ let calculator = {
         return this.a * this.b;
     }
 };
+
+//2
+
+function createPlayer(brand, model) {
+    return {
+        brand: brand,
+        model: model,
+        displayInfo: function() {
+            console.log('brand: ' + this.brand + ' model: ' + this.model);
+        }
+    };
+}
+
+//4
+
+class Player {
+    constructor(brand, model) {
+        (this.brand = brand),
+            (this.model = model),
+            (this.displayInfo = function() {
+                console.log('brand: ' + this.brand + ' model: ' + this.model);
+            });
+    }
+}
