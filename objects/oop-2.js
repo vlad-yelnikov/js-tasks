53ce07c6ac745bc4688f55fc7812dec73c66d330
vlad-yelnikov <vlad.yelnikov@gmail.com>
2019-10-06 18:36:40 +0300

    completed oop-2

--- objects/oop-2.js
+++ objects/oop-2.js
@@ -27,6 +27,27 @@ function createPlayer(brand, model) {
     };
 }
 
+//3
+
+let salaries = {
+    Cris: 150,
+    Brain: 600,
+    John: 300,
+    Steve: 400,
+    Bill: 50
+};
+//spread operator
+function maxSalarie1(obj) {
+    let arr = Object.values(obj);
+    console.log(Math.max(...arr));
+}
+
+//apply
+function maxSalarie2(obj) {
+    let arr = Object.values(obj);
+    console.log(Math.max.apply(null, arr));
+}
+
 //4
 
 class Player {
@@ -37,4 +58,38 @@ class Player {
                 console.log('brand: ' + this.brand + ' model: ' + this.model);
             });
     }
+}
+
+//5
+
+function mul() {
+    let result = 1;
+    let count = 0;
+
+    for (let i = 0; i < arguments.length; i++) {
+        if (typeof arguments[i] === 'number') {
+            result *= arguments[i];
+            count++;
+        }
+    }
+    if (count == 0) {
+        console.log(0);
+    } else {
+        console.log(result);
+    }
+}
+
+//6
+
+let country = {
+    name: 'Украина',
+    language: 'украинский',
+    capital: {
+        name: 'Киев',
+        population: 2907817,
+        area: 847.66
+    }
+};
+function format(beginMsg, endMsg) {
+    console.log(beginMsg + this.name + endMsg);
 }
